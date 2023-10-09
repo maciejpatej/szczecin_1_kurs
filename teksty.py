5d6db8f53e706ffd037f9dd26d94b54c19658628
tomaszkaniecki <kwp@tlen.pl>
2023-10-09 15:05:34 +0200

    9.10 15:06

--- teksty.py
+++ teksty.py
@@ -1,1 +1,29 @@
-print()
+wiek = 2023 - 1983
+str1 = ' to jest tekst 1\n'
+str2 = f'wiek = {wiek}\n'
+str3 = f"""
+**************************
+* to też wiek {wiek}      * 
+* ale ma specjalne opcje *
+* i tu kolejne teksty... * 
+**************************"""
+
+print(str1, str2, str3)
+
+tekst = "Kurs Pythonr"
+print(tekst.lower())
+print(tekst.count("r"))
+print(len(tekst))
+print(tekst[5])
+print(tekst[0:4] + 'y')
+print(tekst[:4])
+print(tekst[4:])
+print(tekst[:10:2])
+
+stary_format = 'Hello %s!'
+print(stary_format % "Tomek")
+
+sredni_format = 'Hello {}!'
+print(sredni_format.format('TOMEK'))
+print(str1, "tu jakiś tekst....", str2)
+
---
+++ wprowadzanie.py
@@ -0,0 +1,8 @@
+imie = input("Jak masz na imię?\n")
+print(f"Cześć {imie}")
+
+liczba1 = int(input("podaj liczbę a"))
+liczba2 = int(input("podaj liczbę b"))
+print("Wynik dodawania liczba a i b = ", liczba1 + liczba2)
+print("Zmienna liczba1 jest w typie", type(liczba1))
+print("Zmienna liczba2 jest w typie", type(liczba2))
