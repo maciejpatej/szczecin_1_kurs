8c8029358826eb60e9a6104a7cc5106c4e754c17
tomaszkaniecki <kwp@tlen.pl>
2023-10-09 12:13:23 +0200

    Initial commit

---
+++ liczby.py
@@ -0,0 +1,18 @@
+import sys
+
+a = 14
+b = -3
+print("Wynik dodawania:", a + b)
+print("Wynik:", a - b)
+print("Wynik:", a * b)
+print("Wynik:", a / b)
+print("Wynik:", a // b)
+print("Wynik:", a ** b)
+print("Wynik:", a % b)
+print("Wynik:", a + True)
+
+pi = 3.14
+radius = 4.5
+area = pi * (radius ** 2)
+print(area)
+print(sys.float_info)
---
+++ start.py
@@ -0,0 +1,10 @@
+print('Cześć Python!')
+# rok = 2023
+# print(type(rok))
+imie = "Tomek"
+print(type(imie))
+temp = 36.6
+print(type(temp))
+czy_jest_zimno = True
+print(type(czy_jest_zimno))
+# komentarz
