d8750172e25771b502c0c4aea34d9415fd248343
tomaszkaniecki <kwp@tlen.pl>
2023-10-10 08:26:54 +0200

    9.10 17:06

---
+++ krotki.py
@@ -0,0 +1,11 @@
+zakupy = ('tort', "piwo")
+print(zakupy[1])
+user = "maciek",
+# user = 648732468234678236748927894238
+print(type(user))
+
+punkty = 43, 66, 89, 12
+print(punkty)
+# a, b, c, d = punkty
+*a, b, c = punkty
+print(type(a))
