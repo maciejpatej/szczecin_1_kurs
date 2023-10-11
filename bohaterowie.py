3ae13665dce30edf0a7ae046885c599b5f8bd6d2
tomaszkaniecki <kwp@tlen.pl>
2023-10-11 16:40:56 +0200

    11.10 16:40

--- bohaterowie.py
+++ bohaterowie.py
@@ -6,6 +6,12 @@ with open('heroes.json', 'r') as f:
 # print(data)
 odczyt1 = data['secretBase']
 odczyt2 = data['members'][0]['powers'][2]
+odczyt3 = data['members'][0]['powers']
+
+print(odczyt2[2:4])
 lista = []
 for index, wartosc in enumerate(data['members']):
     print(index, wartosc)
+
+
+
