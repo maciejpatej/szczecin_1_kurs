a9bb4194cb46088f2774f6cb2592570bcca404fa
tomaszkaniecki <kwp@tlen.pl>
2023-10-11 15:00:17 +0200

    11.10 15:00

---
+++ zad3.py
@@ -0,0 +1,15 @@
+"""
+Twoim zadaniem jest:
+
+Napisać funkcję add_person(name, age),
+która dodaje nową osobę do bazy danych.
+
+Napisać funkcję get_age(name),
+która zwraca wiek osoby o podanym imieniu.
+
+Napisać funkcję update_age(name, new_age),
+która aktualizuje wiek osoby o podanym imieniu.
+
+Napisać funkcję delete_person(name),
+która usuwa osobę o podanym imieniu z bazy danych
+"""
