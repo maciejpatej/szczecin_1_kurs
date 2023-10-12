7cc3b52e3cf3c97c4c186e8c8c28430d67f720f2
tomaszkaniecki <kwp@tlen.pl>
2023-10-12 16:15:26 +0200

    12.10 16:15

--- kurnik/kury.py
+++ kurnik/kury.py
@@ -9,3 +9,8 @@ class Ptak:
 
     def wydaj_odglos(self):
         pass
+
+class Kura(Ptak):
+
+    def lataj(self):
+        print(f"Tutaj {self.gatunek}, ja nie latam i nie poluje (jeszcze)...")
--- start_czwartek.py
+++ start_czwartek.py
@@ -1,7 +1,13 @@
 from ludzie import ob_human2 as ob
 from funkcje import funkcja1 as f1
+from kurnik import kury
 
 czlowiek3 = ob.Human2(176.4, 'k', "Ania")
 czlowiek3.idz_do_roboty()
 
 print(f1.ksero())
+
+ptak1 = kury.Ptak('orzeł bielik', 100)
+ptak1.lataj()
+ptak2 = kury.Kura("kura brojler", 1)
+ptak2.lataj()
