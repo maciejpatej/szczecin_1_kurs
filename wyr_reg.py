a449a4d88aa9a2f87d4b6e86b2fa7ef57e996c84
tomaszkaniecki <kwp@tlen.pl>
2023-10-12 12:55:40 +0200

    12.10 12:55

--- wyr_reg.py
+++ wyr_reg.py
@@ -2,9 +2,15 @@ import re
 
 tekst = "Python to nie tylko gad, ale także język programowania"
 # wynik = re.match(r'', tekst)
-wynik = re.search(r'nie', tekst)
+wzorzec = re.compile(r'ni')
+# wynik = wzorzec.search(tekst)
+wynik = wzorzec.finditer(tekst)
+lista_wyn = list(wynik)
+print(type(wynik))
 # print(f'Dopasowano: {wynik.group()}, początek: {wynik.span()[0]}, koniec: {wynik.span()[1]}')
-if wynik:
-    print(f'Dopasowano: {wynik.group()}, początek: {wynik.start()}, koniec: {wynik.end()}')
+if len(lista_wyn) > 0:
+    print(f'Dopasowano: {len(lista_wyn)}')
+    for w in lista_wyn:
+        print(f'Dopasowano: {w.group()}, początek: {w.start()}, koniec: {w.end()}')
 else:
     print(wynik)
