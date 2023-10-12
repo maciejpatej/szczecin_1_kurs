6e6a3f7d4ebe65309f1bb39320236541c0bbe77b
tomaszkaniecki <kwp@tlen.pl>
2023-10-12 11:43:09 +0200

    12.10 11:42

---
+++ losowanie.py
@@ -0,0 +1,8 @@
+import random
+
+k6 = random.randint(1, 6)
+print("Wynik rzutu kością:", k6)
+
+lista = ["Tomek", 'Ania', 'Mikołaj', 'Albert']
+loteria = random.choice(lista)
+print("Hybrydową Izerę wygrywa:", loteria)
---
+++ z_ftp.py
@@ -0,0 +1,12 @@
+import ftplib
+
+ftp = ftplib.FTP('test.rebex.net')
+logowanie = ftp.login('demo', 'password')
+print(logowanie)
+katalog = ftp.nlst()
+print(katalog)
+plik = ftp.retrbinary('RETR readme.txt', open('plik_z_ftp.txt', 'wb').write)
+print(plik)
+katalog2 = ftp.nlst('pub/example')
+print(katalog2)
+plik2 = ftp.retrbinary('RETR pub/example/KeyGenerator.png', open('grafika.png', 'wb').write)
