063433784003f9fa3e546da30f78318cf6510ce5
tomaszkaniecki <kwp@tlen.pl>
2023-10-12 11:51:20 +0200

    12.10 11:51

--- losowanie.py
+++ losowanie.py
@@ -6,3 +6,6 @@ print("Wynik rzutu kością:", k6)
 lista = ["Tomek", 'Ania', 'Mikołaj', 'Albert']
 loteria = random.choice(lista)
 print("Hybrydową Izerę wygrywa:", loteria)
+
+liczba = random.random()
+print(liczba + 10)
