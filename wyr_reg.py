469390949ad1c1c22cfd935c2cd27f490e597bea
tomaszkaniecki <kwp@tlen.pl>
2023-10-12 14:02:12 +0200

    12.10 14:02

--- wyr_reg.py
+++ wyr_reg.py
@@ -1,17 +1,24 @@
 import re
 
-tekst = "Python to nie tylko gad, ale także język programowania"
-# wynik = re.match(r'', tekst)
-wzorzec = re.compile(r'ni')
-# wynik = wzorzec.search(tekst)
-wynik = wzorzec.finditer(tekst)
-lista_wyn = list(wynik)
-print(type(wynik))
-# print(f'Dopasowano: {wynik.group()}, początek: {wynik.span()[0]}, koniec: {wynik.span()[1]}')
-if len(lista_wyn) > 0:
-    print(f'Dopasowano: {len(lista_wyn)}')
-    for w in lista_wyn:
-        print(f'Dopasowano: {w.group()}, początek: {w.start()}, koniec: {w.end()}')
-else:
-    print(wynik)
-print(re.split(r' ', tekst))
+tekst = "Python to nie tylko gad, ale także język programowania."
+# # wynik = re.match(r'', tekst)
+# wzorzec = re.compile(r'ni')
+# # wynik = wzorzec.search(tekst)
+# wynik = wzorzec.finditer(tekst)
+# lista_wyn = list(wynik)
+# print(type(wynik))
+# # print(f'Dopasowano: {wynik.group()}, początek: {wynik.span()[0]}, koniec: {wynik.span()[1]}')
+# if len(lista_wyn) > 0:
+#     print(f'Dopasowano: {len(lista_wyn)}')
+#     for w in lista_wyn:
+#         print(f'Dopasowano: {w.group()}, początek: {w.start()}, koniec: {w.end()}')
+# else:
+#     print(wynik)
+# print(re.split(r' ', tekst))
+
+wzorzec2 = re.compile(r'.+?')
+print(wzorzec2.findall(tekst))
+
+# https://miroslawmamczur.pl/wyrazenia-regularne-czym-sa-i-jak-pisac-wlasne-regexy/
+
+
