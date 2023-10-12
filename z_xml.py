e6cdeb96afe70b83cc1706b7d4adb5d3ed22267f
tomaszkaniecki <kwp@tlen.pl>
2023-10-12 10:14:46 +0200

    12.10 10:20

---
+++ z_xml.py
@@ -0,0 +1,6 @@
+import re
+import xml.etree.ElementTree as ET
+import requests
+
+url = requests.get('https://api.weatherapi.com/v1/current.xml?key=d863af85e8fa43618f682810220812&q=Lodz&aqi=no'
+print()
