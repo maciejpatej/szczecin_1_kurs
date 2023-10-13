856aad71d80867fcd2486bd4172dc6a2e902abb6
tomaszkaniecki <kwp@tlen.pl>
2023-10-13 11:26:51 +0200

    13.10 11:26

--- odczyt_z_sql.py
+++ odczyt_z_sql.py
@@ -2,6 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect("akwarium.db")
 cursor = connection.cursor()
+new_tank = cursor.execute("UPDATE fish SET tank_number = 3 WHERE name = 'Nemo'")
 rows = cursor.execute("SELECT name,species,tank_number FROM fish").fetchall()
 rows2 = cursor.execute("SELECT name,species,tank_number FROM fish WHERE name = 'Nemo'").fetchall()
 print(rows)
