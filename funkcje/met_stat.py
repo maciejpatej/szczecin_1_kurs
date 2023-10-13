6ac2e5b876dc0b8e4d35f2248a053b84252e4acc
tomaszkaniecki <kwp@tlen.pl>
2023-10-13 09:35:57 +0200

    13.10 09:35

--- funkcje/met_stat.py
+++ funkcje/met_stat.py
@@ -1,7 +1,19 @@
 class Matematyka:
 
-    def dodaj(self, a, b):
+    @staticmethod
+    def dodaj(a, b):
         return a + b
 
-    def odejmij(self, a, b):
+    @staticmethod
+    def odejmij(a, b):
         return a - b
+
+    def pomnoz(self, a, b):
+        return a * b
+
+
+wynik1 = Matematyka.dodaj(5, 3)
+# wynik2 = Matematyka.pomnoz(Matematyka, 3, 4)
+
+print(wynik1)
+# print(wynik2)
