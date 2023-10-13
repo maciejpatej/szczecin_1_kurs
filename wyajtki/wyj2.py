2209a1377ebf5d2cc9cad52aa64a1a6c090a3532
tomaszkaniecki <kwp@tlen.pl>
2023-10-13 10:28:56 +0200

    13.10 10:28

--- wyajtki/wyj2.py
+++ wyajtki/wyj2.py
@@ -13,18 +13,24 @@ invalid_dict2 = [
     {"name": "Ania", 'age': '10'},
     {"name": "Zbyszek", 'age': '17'},
 ]
-def sprawdz_wiek(users,age):
+
+
+def sprawdz_wiek(users, age):
     count = 0
     for user in users:
         try:
-            if int(user['age']) < age:
-                count += 1
+            user_age = int(user['age'])
         except KeyError:
             print(f"Niepoprawne dane: {user}")
         except ValueError:
-            print(f"Niepoprawny wiek: {user}")
+            print(f"Niepoprawny wiek: {user['age']}")
+        else:
+            count += 1 if user_age < age else 0
+        finally:
+            print(f"Wydruk słownika: {user} ")
     return count
 
 
 print(sprawdz_wiek(valid_dict, 11))
 print(sprawdz_wiek(invalid_dict, 11))
+print(sprawdz_wiek(invalid_dict2, 11))
